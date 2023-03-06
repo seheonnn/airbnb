b861d351fbde7eb1b094b2902a781b93343f0e6d
seheonnn <ho78901@naver.com>
2023-03-06 23:26:37 +0900

    과리자 패널 사용자화 필터 추가

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,49 @@
 from django.contrib import admin
 from .models import Review
 
+class Wordfilter(admin.SimpleListFilter):
+    title = "Filter by words!" # 필터 제목
+
+    parameter_name = "word" # 파라미터 이름
+
+    def lookups(self, reqeust, model_admin): # [("url에 표시 되는 이름","패널에 뜨는 이름")]      ex) url : word = good
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+                                # =queryset
+    def queryset(self, request, reviews): # return할 queryset (리뷰들)
+        # # print(self.value()) # url에 있는 parameter= 다음의 값
+        word = self.value() # 모든 필터 조건을 취소하면 self.value()는 None임
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            reviews
+
+class RatingFilter(admin.SimpleListFilter):
+    title = "Filter by Rating!"  # 필터 제목
+
+    parameter_name = "rating"  # 파라미터 이름
+
+    def lookups(self, reqeust, model_admin):  # [("url에 표시 되는 이름","패널에 뜨는 이름")]      ex) url : potato = good
+         return [
+            ("good", "Good"),
+            ("bad", "Bad"),
+        ]
+        # =queryset
+    def queryset(self, request, reviews):  # return 할 queryset (리뷰들)
+        rate = self.value()
+        if rate == "good":
+            return reviews.filter(rating__gte=3)
+        elif rate == "bad":
+            return reviews.filter(rating__lt=3)
+        else:
+            return reviews
+
+
+
+
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
@@ -9,6 +52,14 @@ class ReviewAdmin(admin.ModelAdmin):
         "payload",
     )
     list_filter = (
+        Wordfilter,
+        RatingFilter,
         "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
     )
+
+
+
 # Register your models here.
