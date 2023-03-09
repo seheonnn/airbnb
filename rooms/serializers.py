ef03363358892ddddbabd438cda729f46b34cf9d
seheonnn <ho78901@naver.com>
2023-03-09 21:31:00 +0900

    serializer context & Review revers serializers

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,7 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Amenity, Room
 from users.serializer import TinyUserSerializer
 from categories.serializers import CategorySerializer
+from reviews.serializers import ReviewSerializer
 
 
 
@@ -12,22 +13,40 @@ from categories.serializers import CategorySerializer
 #         depth = 1 # owner에 userid가 들어가는 것이 아닌 user object가 들어감 -> 데이터가 너무 많아짐
 
 
-class AmenitySerializer(ModelSerializer):
+class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Amenity
         fields = "name", "description"
 
-class RoomDetailSerializer(ModelSerializer):
+class RoomDetailSerializer(serializers.ModelSerializer):
 
     owner = TinyUserSerializer(read_only=True)
     amenities = AmenitySerializer(read_only=True, many=True) # amenity 여러 개 many= 추가
     category = CategorySerializer(read_only=True)
+
+    # rating 값을 return하는 method를 만들 것임.
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField() # 인스타그램에서 is_liked(좋아요)로 사용, 값에 따라 빨간 하트, 빈 하트
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+
     class Meta:
         model = Room
         fields = "__all__"
         # depth=1
 
-class RoomListSerializer(ModelSerializer):
+    def get_rating(self, room): # 이름은 항상 'get_값 이름'
+        print(self.context)
+        return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context['request'] # context= 를 통해 넘어온 데이터 사용
+        return room.owner == request.user
+
+class RoomListSerializer(serializers.ModelSerializer):
+
+    rating = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
     class Meta:
         model = Room
         fields = (
@@ -36,4 +55,12 @@ class RoomListSerializer(ModelSerializer):
             "country",
             "city",
             "price",
+            "rating",
+            "is_owner",
         )
+    def get_rating(self, room):
+        return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context['request']  # context= 를 통해 넘어온 데이터 사용
+        return room.owner == request.user
