935254e2c70850f6574fb2318987780eea0e4d18
seheonnn <ho78901@naver.com>
2023-03-15 12:15:30 +0900

    REFACTOR: GraphQL

--- config/schema.py
+++ config/schema.py
@@ -13,7 +13,9 @@ import strawberry
 
 import typing # 코드에 type annotation 을 추가할 수 있게 해줌
 
-# 영화 API
+# type, resolver, Query class, Mutation 별로 각각 별도의 파일로 분리 가능
+
+# 영화 API type
 @strawberry.type
 class Movie:
     pk: int
@@ -25,16 +27,24 @@ movies_db = [
     Movie(pk=1, title="Godfather", year=1990, rating=10),
 ]
 
+
+def movies(): # class 안에 있는 것이 아니기 때문에 self는 필요 없음
+    return movies_db
+def movie(movie_pk:int):
+    return movies_db[movie_pk - 1]
 # resolver
 @strawberry.type
 class Query:
-    @strawberry.field
-    def movies(self) -> typing.List[Movie]:
-        return movies_db
+    # @strawberry.field
+    # def movies(self) -> typing.List[Movie]:
+    #     return movies_db
 
-    @strawberry.field
-    def movie(self, movie_pk:int) -> Movie:
-        return movies_db[movie_pk - 1]
+    # @strawberry.field
+    # def movie(self, movie_pk:int) -> Movie:
+    #     return movies_db[movie_pk - 1]
+    movies: typing.List[Movie] = strawberry.field(resolver=movies)
+    movie: Movie = strawberry.field(resolver=movie)
+
 # 모든 영화 검색
 # {
 #   movies{
@@ -54,14 +64,21 @@ class Query:
 #   }
 # }
 
-# mutation
-@strawberry.type
-class Mutation:
-    @strawberry.mutation
-    def add_movie(self, title:str, year:int, rating:int) -> Movie:
+def add_movie(title:str, year:int, rating:int):
         new_Movie = Movie(pk=len(movies_db)+1, title=title, year=year, rating=rating)
         movies_db.append(new_Movie)
         return new_Movie
+# mutation
+@strawberry.type
+class Mutation:
+    # @strawberry.mutation
+    # def add_movie(self, title:str, year:int, rating:int) -> Movie:
+    #     new_Movie = Movie(pk=len(movies_db)+1, title=title, year=year, rating=rating)
+    #     movies_db.append(new_Movie)
+    #     return new_Movie
+    add_movie: Movie = strawberry.mutation(resolver=add_movie)
+
+
 # 새 양화 추가
 # mutation {
 #   addMovie(title:"Godfather II", year:1995, rating:9){
