6e980e577b670d15c7712fc5072947858b7d5ec1
seheonnn <ho78901@naver.com>
2023-03-17 12:07:43 +0900

    TEST: room api test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from . import models
+from users.models import User
 
 class TestAmenities(APITestCase):
     # def test_two_plus_two(self):
@@ -82,3 +83,25 @@ class TestAmenity(APITestCase):
     def test_delete_amenity(self):
         response = self.client.delete("/api/v1/rooms/amenities/1")
         self.assertEqual(response.status_code, 204)
+
+
+class TestRooms(APITestCase):
+    # get 부분은 인증 없이 가능
+    # post 부분은 로그인 필요
+    def setUp(self): # join
+        user = User.objects.create(username="test")
+        user.set_password("123")
+        user.save()
+        self.user = user
+    def test_create_room(self):
+        response = self.client.post("/api/v1/rooms/")
+        self.assertEqual(response.status_code, 403)
+
+        # login
+        # self.client.login(username="test", password="123")
+        self.client.force_login(self.user)
+
+        response = self.client.post("/api/v1/rooms/")
+        print(response.json())
+
+
--- rooms/views.py
+++ rooms/views.py
@@ -108,7 +108,7 @@ class Rooms(APIView):
             except Exception:
                 raise ParseError("Amenity not found")
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
         # else:
         #     raise NotAuthenticated
 
