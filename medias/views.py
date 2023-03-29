f312a6df5426df056fcabf70c84208f5d9fc528f
seheonnn <ho78901@naver.com>
2023-03-29 11:58:12 +0900

    feat: upload Image(CloudFlare)

--- medias/views.py
+++ medias/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+import requests
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.permissions import IsAuthenticated
+
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 from .models import Photo
 
@@ -24,3 +28,30 @@ class PhotoDetail(APIView):
             raise PermissionDenied
         photo.delete()
         return Response(status=HTTP_200_OK)
+
+class GetUploadURL(APIView):
+    def post(self, request):
+        from config import settings
+        url = f"https://api.cloudflare.com/client/v4/accounts/{settings.CF_ID}/images/v2/direct_upload"
+        one_time_url = requests.post(
+            url,
+            headers={
+                "Authorization":f"Bearer {settings.CF_TOKEN}"
+            },
+        )
+        one_time_url = one_time_url.json()
+        result = one_time_url.get("result")
+        if result:
+            return Response({"id": result.get("id"), "uploadURL": result.get("uploadURL")})
+        else:
+            return Response({
+                "id": "00000",
+                "uploadURL": "http://127.0.0.1:8000/api/v1/medias/photos/upload-photo",
+            })
+
+
+class UploadPhoto(APIView):
+    def post(self, request):
+        image_data = request.FILES["file"]
+        path = default_storage.save(image_data, ContentFile(image_data.read()))
+        return Response({"result": {"id": path}})
